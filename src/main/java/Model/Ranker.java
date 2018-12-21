4c23786b9a30b429d540366923b991bae8f7c9f2
Hen1234 <henmor93@gmail.com>
2018-12-21 17:35:13 +0200

    cosSim in Ranker

--- src/main/java/Model/Ranker.java
+++ src/main/java/Model/Ranker.java
@@ -6,13 +6,14 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Ranker {
+
     private PriorityQueue<QueryDoc> qDocQueue;
     private int queryLength;
 
 
     public Ranker() {
         qDocQueue = new PriorityQueue<QueryDoc>();
-        queryLength=0;
+        queryLength = 0;
     }
 
     public PriorityQueue<QueryDoc> getqDocQueue() {
@@ -23,7 +24,7 @@ public class Ranker {
         this.qDocQueue = qDocQueue;
     }
 
-    public void getQueryDocFromSearcher(QueryDoc currentQueryDoc, int queryLength){
+    public void getQueryDocFromSearcher(QueryDoc currentQueryDoc, int queryLength) {
 
 
         //set the queryLength
@@ -36,11 +37,15 @@ public class Ranker {
             Map.Entry pair = (Map.Entry) it.next();
             QueryTerm currentQueryTerm = (QueryTerm) pair.getValue();
             //System.out.println(currentQueryDoc.getDocNO()+"rank= "+currentQueryDoc.getRank());
-            currentQueryDoc.setRank(currentQueryDoc.getRank()+BM25func(currentQueryTerm, currentQueryDoc));
+            currentQueryDoc.setRank(currentQueryDoc.getRank() + 0.75*BM25func(currentQueryTerm, currentQueryDoc));
             //System.out.println(currentQueryDoc.getDocNO()+"rank= "+currentQueryDoc.getRank());
 
 
+
         }
+
+        //update the currentQueryDoc's rank by cosSim
+        currentQueryDoc.setRank(currentQueryDoc.getRank() + 0.25*cosSim(currentQueryDoc, queryLength));
         qDocQueue.add(currentQueryDoc);
     }
 
@@ -55,25 +60,51 @@ public class Ranker {
 
         //k=2, B=0.75
 
-        return Math.log10((M+1)/df)*cwq*((3*cwd)/(cwd+(2*(0.25+(0.75*(d/avdl))))));
+        return Math.log10((M + 1) / df) * cwq * ((3 * cwd) / (cwd + (2 * (0.25 + (0.75 * (d / avdl))))));
 
     }
 
-    private double cosSim(QueryTerm currentQueryTerm, QueryDoc currentQueryDoc) {
+    private double cosSim(QueryDoc currentQueryDoc, int queryLength) {
 
-        int cwq = currentQueryTerm.getAppearanceInQuery(); //amount of appearances in the query
-        int cwd = currentQueryTerm.getDocsAndAmount().get(currentQueryDoc.getDocNO()); //amount of appearances in the doc
-        int d = currentQueryDoc.getLength(); //length of the doc
-        int dQuery = queryLength;
-        int df = currentQueryTerm.getDf();  //the df of the term in the corpus
-        double avdl = Searcher.avdl;
-        int N = Searcher.numOfDocumentsInCorpus; // number of docs in corpus
+        //iterator for the QueryTermsInTheQueryDoc
+        Iterator it = currentQueryDoc.getQueryTermsInDocsAndQuery().entrySet().iterator();
+
+        int cwq=0;   //amount of appearances in the query
+        int cwd=0;    //amount of appearances in the doc
+        int d=0;      //length of the doc
+        int dQuery=0;
+        int df=0;     //the df of the term in the corpus
+        double avdl=0;
+        int N=0;     // number of docs in corpus
+        avdl = Searcher.avdl;
+        N = Searcher.numOfDocumentsInCorpus; // number of docs in corpus
+        d = currentQueryDoc.getLength(); //length of the doc
+        dQuery = queryLength;
+
+        double Mone=0;
+        double Mechane =0;
+        double firstSigmaMechane =0;
+        double secondSigmaMechane =0;
+
+        while (it.hasNext()) {
+
+            Map.Entry pair = (Map.Entry) it.next();
+            QueryTerm currentQueryTerm = (QueryTerm) pair.getValue();
+
+            cwq = currentQueryTerm.getAppearanceInQuery(); //amount of appearances in the query
+            cwd = currentQueryTerm.getDocsAndAmount().get(currentQueryDoc.getDocNO()); //amount of appearances in the doc
+            df = currentQueryTerm.getDf();  //the df of the term in the corpus
+
+            Mone = Mone+ ((cwd / d) * (Math.log10(N / df)) * (cwq/dQuery) * (Math.log10(N / df)));
+            firstSigmaMechane = firstSigmaMechane+ Math.pow((cwd / d) * (Math.log10(N / df)),2);
+            secondSigmaMechane = secondSigmaMechane+ Math.pow((cwq/dQuery) * (Math.log10(N / df)),2);
 
 
-        double Mone = (cwd/d) * (Math.log10(N/df));
+        }
 
-        return 0;
-
+        Mechane = Math.pow(firstSigmaMechane*secondSigmaMechane,(1/2));
+        System.out.println("RankCosSim= "+Mone/Mechane);
+        return (Mone/Mechane);
     }
 
 
--- src/main/java/Model/main.java
+++ src/main/java/Model/main.java
@@ -4,10 +4,11 @@ public class main {
 
     public static void main(String[] args) {
 
-        Parse p = new Parse();
-        String query = "I want 55 Billion";
-        String afterParse = p.parser(null,query,true, true);
-        System.out.println(afterParse);
+//        Parse p = new Parse();
+//        String query = "I want 55 Billion";
+//        String afterParse = p.parser(null,query,true, true);
+        //System.out.println(afterParse);
+        System.out.println(Math.log10(2/3));
 
     }
 
